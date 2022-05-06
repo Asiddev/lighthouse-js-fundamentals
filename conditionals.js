5148a4978c7e75fdd27127af3675acb74d34c43d
asiddev <AsidDev@protonmail.com>
2022-05-06 04:21:15 +0000

    Add more conditional examples

---
+++ conditionals.js
@@ -0,0 +1,67 @@
+// const raining = true;
+// let cold = false;
+
+// if (raining) {
+//   console.log("Don't forget your umbrella!");
+// }
+
+// if (cold) {
+//   console.log("Make sure you pick out a scarf!");
+// }
+
+// console.log("Now you're ready to go outside!");
+
+// let cold = false;
+
+// if (cold) {
+//   console.log("Make sure you pick out a scarf!");
+// } else {
+//   console.log("Short sleeves are fine.");
+// }
+
+// //
+
+// const raining = true;
+
+// if (raining) {
+//   console.log("Don't forget your umbrella!");
+// }
+
+// const raining = true;
+// let cold = false;
+
+// if (raining) {
+//   console.log("Don't forget your umbrella!");
+// }
+
+// if (cold) {
+//   console.log("Make sure you pick out a scarf!");
+// }
+
+// console.log("Now you're ready to go outside!");
+
+// let cold = false;
+
+// if (cold) {
+//   console.log("Make sure you pick out a scarf!");
+// } else {
+//   console.log("Short sleeves are fine.");
+// }
+// const temperature = 12;
+
+// if (temperature < 0) {
+//   console.log("Make sure you pick out a scarf!");
+// } else if (temperature < 15) {
+//   console.log("Short sleeves won't cut it!");
+// } else {
+//   console.log("Short sleeves are fine.");
+// }
+
+// console.log("Now you're ready to go outside!");
+
+const isCitizen = true;
+const age = 10;
+
+if (isCitizen && age > 18) {
+  console.log("You are eligible to vote.");
+}
